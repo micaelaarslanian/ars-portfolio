a100c0611bbf8b96f7614be9fa637931d7dfae8e
Micaela Arslanian <micaelarslanian@gmail.com>
2025-12-19 12:09:04 +0000

    fix ease

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,7 @@ const LINKS = [
 ];
 
 // animation ease
-const EASE = [0.34, 1.56, 0.64, 1];
+const EASE: [number, number, number, number] = [0.34, 1.56, 0.64, 1];
 
 const PANEL_EASE: [number, number, number, number] = [0.4, 0, 0.2, 1];
 
